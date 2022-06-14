529faa4b0c9c72f585f77f60861f506e41b059e6
abyss <ray5209a26@gmail.com>
2022-06-14 15:26:53 +0800

    改print方式

--- final_program_new_version/print.c
+++ final_program_new_version/print.c
@@ -7,14 +7,17 @@ extern int num;
 void my_print(struct basic_account* HEAD){
 
     struct basic_account *first;
-    printf("name\ttotal\tdate for the lastest trade\n");
+    printf("name\t\ttotal\t\tdate for the lastest trade\n");
     first=HEAD;
     while(first->next){
         struct Information *tail;
         first = first->next;
         tail = first->trade;
         while(tail->nt){tail = tail->nt;}
-        printf("%s\t%d\t%s\n", first->name, first->money - first->trade->loan, tail->date);
+        if(strlen(first->name)>=8)
+          printf("%s\t%d\t\t%s\n", first->name, first->money - first->trade->loan, tail->date);
+        else
+          printf("%s\t\t%d\t\t%s\n", first->name, first->money - first->trade->loan, tail->date);
     }
     int state;
     printf("---output file for above data? 1.YES 2.NO---\n");
@@ -42,25 +45,49 @@ void my_print(struct basic_account* HEAD){
 }
 void my_print_inform(struct Information *HEAD){
     struct Information *first;
-    printf("YYYY/MM/DD\t\t MOENY\t\tTOTAL\t\tRECORD\n");
+    printf("YYYY/MM/DD\t MOENY\t\tTOTAL\t\tRECORD\n");
     for(first=HEAD;first!=NULL;first=first->nt){
-      if(first->used_money>=0){
-        printf("%s\t\t+%d\t\t%d\t\t%s\n",first->date, first->used_money, first->total, first->ST);
+      // if(first->used_money>=0){
+      //   printf("%s\t\t+%d\t\t%d\t\t%s\n",first->date, first->used_money, first->total, first->ST);
+      // }
+      // else {
+      //   printf("%s\t\t-%d\t\t%d\t\t%s\n",first->date, first->used_money*(-1), first->total, first->ST);
+      // }
+      if(first->used_money>999999){
+        if(first->total>9999999){
+        printf("%s\t+%d\t%d\t%s\n",first->date, first->used_money, first->total, first->ST);
+        }
+        else if(first->total>0){
+          printf("%s\t+%d\t%d\t\t%s\n",first->date, first->used_money, first->total, first->ST);
+        }
+        //printf("%s\t+%d\t%d\t%s\n",first->date, first->used_money, first->total, first->ST);
       }
-      else {
-        printf("%s\t\t-%d\t\t%d\t\t%s\n",first->date, first->used_money*(-1), first->total, first->ST);
+      else if(first->used_money>0){
+        if(first->total>9999999){
+        printf("%s\t+%d\t\t%d\t%s\n",first->date, first->used_money, first->total, first->ST);
+        }
+        else if(first->total>0){
+          printf("%s\t+%d\t\t%d\t\t%s\n",first->date, first->used_money, first->total, first->ST);
+        }
+        //printf("%s\t+%d\t\t%d\t%s\n",first->date, first->used_money, first->total, first->ST);
       }
-      // if(first->used_money>999999){
-      //   printf("%s\t+%d\t%d\t%s\n",first->date, first->used_money, first->total, first->ST);
-      // }
-      // else if(first->used_money>0){
-      //   printf("%s\t+%d\t\t%d\t%s\n",first->date, first->used_money, first->total, first->ST);
-      // }
-      // else if(first->used_money<-999999){
-      //   printf("%s\t-%d\t%d\t%s\n",first->date, first->used_money*(-1), first->total, first->ST);
-      // }
-      // else{
-      //   printf("%s\t-%d\t\t%d\t%s\n",first->date, first->used_money*(-1), first->total, first->ST);
-      // }
+      else if(first->used_money<-999999){
+        if(first->total>9999999){
+        printf("%s\t-%d\t%d\t%s\n",first->date, first->used_money, first->total, first->ST);
+        }
+        else if(first->total>0){
+          printf("%s\t-%d\t%d\t\t%s\n",first->date, first->used_money, first->total, first->ST);
+        }
+        //printf("%s\t-%d\t%d\t%s\n",first->date, first->used_money*(-1), first->total, first->ST);
+      }
+      else{
+        if(first->total>9999999){
+        printf("%s\t-%d\t\t%d\t%s\n",first->date, first->used_money, first->total, first->ST);
+        }
+        else if(first->total>0){
+          printf("%s\t-%d\t\t%d\t\t%s\n",first->date, first->used_money, first->total, first->ST);
+        }
+        //printf("%s\t-%d\t\t%d\t%s\n",first->date, first->used_money*(-1), first->total, first->ST);
+      }
     }
 }
