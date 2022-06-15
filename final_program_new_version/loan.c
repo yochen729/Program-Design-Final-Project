93a46eaa1d35d3706f662c3bcf84890e70272806
yochen729 <103554434+yochen729@users.noreply.github.com>
2022-06-16 01:26:37 +0800

    loan time add but borrow error not fix

--- final_program_new_version/loan.c
+++ final_program_new_version/loan.c
@@ -9,12 +9,18 @@ void my_loan(struct basic_account* HEAD, char *Name){
     }
     
     system("cls");
+    time_t now; // 變數宣告
+    time(&now); // 取得現在的日期時間
+    struct tm *p=gmtime(&now);
+    char year[5],mon[3],day[3];
+    char DAY[MAX_DATE];
+    sprintf(DAY,"%d/%d/%d",p->tm_year+1900,p->tm_mon+1,p->tm_mday);
     struct basic_account *list=AccountCheck(HEAD,Name);
     struct Information *new_data = malloc(sizeof(struct Information));
     struct Information *first = list->trade;
     while(first->nt){first = first->nt;}
     
-    int status;char day[MAX_DATE];
+    int status;
     printf("1.Borrow\t2.Pay Off\n");
     scanf("%d", &status);
     if (status == 2 && first->loan == 0)
@@ -34,7 +40,7 @@ void my_loan(struct basic_account* HEAD, char *Name){
             printf("Please pay off your money first.\n");
         else
         {
-            strcpy(new_data->ST, "BORROW"), strcpy(new_data->date, day);
+            strcpy(new_data->ST, "BORROW"), strcpy(new_data->date, DAY);
             new_data->used_money = amount, new_data->loan = first->loan + amount, list->money = new_data->total = list->money + amount;
             first->nt = new_data, new_data->nt = NULL;
 
@@ -52,15 +58,15 @@ void my_loan(struct basic_account* HEAD, char *Name){
         {
             printf("All Pay Off.\n");
             printf("You Pay Off %d, remaining amount in your account is $%d.\n", amount, list->money - first->loan);
-            strcpy(new_data->ST, "PAYOFF"), strcpy(new_data->date, day);
-            new_data->used_money = amount * (-1), new_data->loan = 0, list->money = new_data->total = first->total - first->loan;
+            strcpy(new_data->ST, "PAYOFF"), strcpy(new_data->date, DAY);
+            new_data->used_money = first->loan * (-1), new_data->loan = 0, list->money = new_data->total = first->total - first->loan;
             first->nt = new_data, new_data->nt = NULL;
         }
         else
         {
             printf("You Pay Off %d, remaining amount in your account is $%d.\n", amount, list->money - amount);
             printf("Remain $%d to pay off.\n", first->loan - amount);
-            strcpy(new_data->ST, "PAYOFF"), strcpy(new_data->date, day);
+            strcpy(new_data->ST, "PAYOFF"), strcpy(new_data->date, DAY);
             new_data->used_money = amount * (-1), new_data->loan = first->loan - amount, list->money = new_data->total = first->total - amount;
             first->nt = new_data, new_data->nt = NULL;
         }
